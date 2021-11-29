19da1a4859ef040f6d28b8c2f8f4a1f3550ee6b5
UVLines <UVLines001@gmail.com>
2021-11-29 21:49:18 +0100

    Minting + supply fetching works

--- src/App.js
+++ src/App.js
@@ -8,10 +8,12 @@ function App() {
   const CONTRACT_ADDRESS = "0x77C9e7733550026AcE28950e973681C0F74191E3";
   const [contract, setContract] = useState(undefined);
   const [ethAddress, setEthAddress] = useState("");
-  // const [totalSupply, setTotalSupply] = useState(0);
-  let selectedAccount;
+  const [totalSupply, setTotalSupply] = useState(0);
+  const totalNfts = 8888;
+  // let selectedAccount;
   // let nftContract;
-  async function connectWallet() {
+
+  const connectWallet = async () => {
     let provider = window.ethereum;
 
     if (typeof provider !== "undefined") {
@@ -20,47 +22,100 @@ function App() {
       provider
         .request({ method: "eth_requestAccounts" })
         .then((accounts) => {
-          selectedAccount = accounts[0];
-          console.log(`Selected account is ${selectedAccount}`);
+          // selectedAccount = accounts[0];
+          // console.log(`Selected account is ${selectedAccount}`);
           setEthAddress(accounts[0]);
         })
         .catch((err) => {
           console.log(err);
         });
     }
+  };
 
-    // web3.
-  }
+  const disconnectWallet = () => {
+    let provider = window.ethereum;
 
-  async function loadBlockchain() {
+    if (typeof provider === "undefined") {
+      // MetaMask is installed
+      setEthAddress("");
+      // provider.request({ method: "eth_requestAccounts" });
+      console.log("disconeccted");
+    }
+  };
+
+  const loadBlockchain = async () => {
     let provider = window.ethereum;
     const web3 = new Web3(provider);
+    // const network = await web3.eth.getChainId();
+    // console.log("chainID: " + network);
 
-    const nftContract = new web3.eth.Contract(
+    const contract = new web3.eth.Contract(
       MintingContract.abi,
       CONTRACT_ADDRESS
     );
-    setContract(nftContract);
-    // const totalSupply = await contract.methods.currentSupply().call();
-    console.log(nftContract);
-  }
+    setContract(contract);
+  };
+
+  useEffect(() => {
+    connectWallet();
+    getSupply();
+  });
+
   useEffect(() => {
     loadBlockchain();
   }, [window.ethereum]);
+
   const buyFlys = (number) => {
     if (contract !== undefined && ethAddress !== "") {
-      console.log(number);
+      // console.log(number);
       var value = number * 20000000;
-      console.log(value);
-      console.log(selectedAccount);
-      contract.methods.buyFlys(number).send({ from: ethAddress, value: value });
+      // console.log(value);
+      // console.log(ethAddress);
+
+      contract.methods
+        .buyFlys(number)
+        .send({ from: ethAddress, value: value })
+        .then((tx) => {
+          // console.log(tx);
+          alert(
+            "Transcation hash: " +
+              "https://ropsten.etherscan.io/tx/" +
+              tx.transactionHash
+          );
+          getSupply();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+
+  const getSupply = () => {
+    if (contract !== undefined && ethAddress !== "") {
+      contract.methods
+        .currentSupply()
+        .call()
+        .then((supply) => {
+          setTotalSupply(supply);
+          // console.log(supply);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
   };
 
   return (
     <div>
-      <Nav connectWallet={connectWallet} />
+      <Nav
+        connectWallet={connectWallet}
+        disconnectWallet={disconnectWallet}
+        ethAddress={ethAddress}
+        // style={{ backgroundColor: "red" }}
+      />
       <button onClick={() => buyFlys(5)}>Mint</button>
+      <button onClick={() => getSupply()}>refreshSupply</button>
+      <h4>{`${totalSupply} / ${totalNfts}`}</h4>
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,19 @@
 import React from "react";
-// import Web3 from "web3";
 
-function Nav({ connectWallet }) {
+function Nav({ connectWallet, disconnectWallet, ethAddress }) {
   return (
-    <div>
-      <h3>nav</h3>
-      <button onClick={() => connectWallet()}>connect nav</button>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-around",
+        alignItems: "flex-end",
+        marginLeft: "40vw",
+        backgroundColor: "red",
+      }}
+    >
+      {/* <h3>nav</h3> */}
+      <button onClick={() => connectWallet()}>{ethAddress}</button>
+      <button onClick={() => disconnectWallet()}>disconnect</button>
     </div>
   );
 }
